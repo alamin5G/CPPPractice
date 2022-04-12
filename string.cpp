c1414e35de8dc93f09a7f5b182ad7a54f6e98f78
alamin5g <alaminrakib4864@gmail.com>
2022-04-13 03:36:35 +0600

    string manipulation

---
+++ string.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string st = "A simple string can find from 32 no house.";
+    int size = st.size();
+    st[0]='B';
+    
+
+    cout << size << endl;
+    cout << st << endl;
+
+    
+    return 0;
+}
