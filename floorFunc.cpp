e98abc66638cb6d55151b8b24f516b78244dd792
Md. Alamin <alaminrakib4864@gmail.com>
2022-09-21 04:14:40 +0600

    floor function

---
+++ floorFunc.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file floorFunc.cpp
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-09-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    //here we will check
+    //if
+    float a = 3.2;
+    float b = -3.2;
+    //then what will be the output?
+    int af = floor(a); //output 3
+    int bf = floor(b); //output -4
+
+    cout << af << endl;
+    cout << bf << endl;
+    //so we can say floor is working with lowest value;
+    return 0;
+}
